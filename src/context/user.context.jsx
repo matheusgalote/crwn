3ca845bc90bd6f071090d563b70fe287a35a0083
matheusgalote <mateugalote@gmail.com>
2022-11-03 21:40:22 -0300

    Adicionando funcao de reducer

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,29 +1,61 @@
-import { createContext, useState, useEffect } from "react";
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase";
+import { createContext, useEffect, useReducer } from "react";
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebase";
 
 export const UserContext = createContext({
   setCurrentUser: (user) => null,
   currentUser: null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "SET_CURRENT_USER":
+      return {
+        currentUser: payload,
+      };
+    case "increment":
+      return {
+        value: state.value + 1,
+      };
+    default:
+      throw new Error(`ERROR TYPE ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = {currentUser, setCurrentUser};
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [ state, dispatch ] = useReducer(userReducer, INITIAL_STATE);
+
+  const { currentUser } = state;
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+  }
+
+  const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      if(user) {
+      if (user) {
         createUserDocumentFromAuth(user, {});
       }
-      setCurrentUser(user)
+      setCurrentUser(user);
     });
 
     return unsubscribe;
   }, []);
 
-  return (
-  <UserContext.Provider value={value}>
-    {children}
-  </UserContext.Provider>
-  );
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
